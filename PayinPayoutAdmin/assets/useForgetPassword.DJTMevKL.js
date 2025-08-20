2fe3fc4cd805a05b5a5874b3f92a3d4b6f3c9e5e
rampravesh yadav <rampraveshkcse@gmail.com>
2025-08-20 23:17:52 +0530

    changes

--- PayinPayoutAdmin/assets/useForgetPassword.DJTMevKL.js
+++
@@ -1,1 +0,0 @@
-import{b as s,I as n}from"./index.Mkiy1plC.js";const r=()=>s({mutationFn:async t=>{const{data:a}=await n.post("/request-otp",t);return a}}),e=()=>s({mutationFn:async t=>{const{data:a}=await n.post("/verify-otp",t);return a}}),u=()=>s({mutationFn:async t=>{const{data:a}=await n.post("/forgot-password",t);return a}});export{e as a,u as b,r as u};
