020b182a337c974c4f49e628724238686cca0908
rampravesh yadav <rampraveshkcse@gmail.com>
2025-08-21 17:14:52 +0530

    changes

---
+++ PayinPayoutAdmin/assets/useMpin.3mzRVsDd.js
@@ -0,0 +1,1 @@
+import{b as a,c as e}from"./index.Ct2vEYE0.js";const i=()=>a({mutationFn:async t=>{const{data:n}=await e.post("/change/mpin",t);return n}}),r=()=>a({mutationFn:async t=>{const{data:n}=await e.post("/verify-mpin/",t);return n}});export{i as a,r as u};
