22d1a59a67b63f2b1c9c94c01a7e63912566422c
Niraj Singh <tech@unibcomp.net>
2025-08-29 18:08:36 +0530

    changes

---
+++ PayinPayoutAdmin/assets/useDeleteAndSuspendUser.C9U99ZFi.js
@@ -0,0 +1,1 @@
+import{i as t,k as n,l as u}from"./index.BlDP0ua6.js";const i=()=>{const e=t();return n({mutationFn:async s=>(await u.post("/user/delete-and-suspend-user",s)).data,mutationKey:["delete-and-suspend-user"],onSuccess:()=>{e.invalidateQueries({queryKey:["Allusers"]})}})};export{i as u};
