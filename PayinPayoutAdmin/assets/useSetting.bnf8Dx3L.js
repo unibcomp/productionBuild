cc3bb355ffdfc96558114b88731faecd1227dd33
Niraj Singh <tech@unibcomp.net>
2025-08-30 17:23:39 +0530

    changes

---
+++ PayinPayoutAdmin/assets/useSetting.bnf8Dx3L.js
@@ -0,0 +1,1 @@
+import{k as o,o as u}from"./index.D2TMnlCu.js";const s=e=>u.get("/get-setting",{params:e}).then(t=>t.data),g=(e={})=>{const{data:t,isLoading:n,refetch:i}=o({queryKey:["setting",e],queryFn:()=>s(e),enabled:!!(e!=null&&e.user_id),refetchOnWindowFocus:!1});return{data:t==null?void 0:t.data,isLoading:n,refetch:i}};export{g as u};
