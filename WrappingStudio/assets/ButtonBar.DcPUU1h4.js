a6740bbcb22601a034eda6896098f2d594997335
Niraj Singh <tech@unibcomp.net>
2025-10-06 11:35:01 +0530

    changes

---
+++ WrappingStudio/assets/ButtonBar.DcPUU1h4.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index.C1zK8HYA.js";const o=({buttons:e})=>a.jsx("div",{className:"flex gap-2",children:e.map((s,l)=>a.jsx("button",{onClick:s.onClick,className:`px-3   text-white rounded-lg transition ${s.colorClass}`,children:s.label},l))});export{o as B};
---
+++ WrappingVendor/assets/Careers.CVr-OUYQ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index.Cj5zF2mn.js";const s=()=>r.jsx(r.Fragment,{children:"Careers Page"});export{s as default};
