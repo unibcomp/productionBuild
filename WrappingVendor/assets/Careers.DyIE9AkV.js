0c13f7232e5dbc0f3a6b5dd7f9f977e2878f6c4b
Niraj Singh <tech@unibcomp.net>
2025-11-21 10:53:44 +0530

    new build file

---
+++ WrappingVendor/assets/Careers.DyIE9AkV.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index.DlsZ5bxu.js";const s=()=>r.jsx(r.Fragment,{children:"Careers Page"});export{s as default};
---
+++ WrappingVendor/assets/user-pen.Bhy7OPlU.js
@@ -0,0 +1,11 @@
+import{c as e}from"./index.DlsZ5bxu.js";/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=[["rect",{width:"16",height:"20",x:"4",y:"2",rx:"2",ry:"2",key:"76otgf"}],["path",{d:"M9 22v-4h6v4",key:"r93iot"}],["path",{d:"M8 6h.01",key:"1dz90k"}],["path",{d:"M16 6h.01",key:"1x0f13"}],["path",{d:"M12 6h.01",key:"1vi96p"}],["path",{d:"M12 10h.01",key:"1nrarc"}],["path",{d:"M12 14h.01",key:"1etili"}],["path",{d:"M16 10h.01",key:"1m94wz"}],["path",{d:"M16 14h.01",key:"1gbofw"}],["path",{d:"M8 10h.01",key:"19clt8"}],["path",{d:"M8 14h.01",key:"6423bh"}]],d=e("building",h);/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M11.5 15H7a4 4 0 0 0-4 4v2",key:"15lzij"}],["path",{d:"M21.378 16.626a1 1 0 0 0-3.004-3.004l-4.01 4.012a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506z",key:"1817ys"}],["circle",{cx:"10",cy:"7",r:"4",key:"e45bow"}]],y=e("user-pen",t);export{d as B,y as U};
