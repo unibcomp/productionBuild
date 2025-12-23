90d9eeca5b2f235c9041b13998ea4b3742d84509
rampravesh <rampraveshkcse@gmail.com>
2025-12-23 15:20:38 +0530

    build

--- WrappingStudio/assets/unibcomp.BpPk2ZWr.js
+++
@@ -1,1 +0,0 @@
-const o="/assets/unibcomp.BJaHTGOL.svg";export{o as u};
---
+++ WrappingStudio/assets/useModal.Bv9tDr7o.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index.BQOoNfp6.js";const u=()=>{const[t,s]=o.useState({}),[a,l]=o.useState(!1),[n,c]=o.useState();return{isModalOpen:t,openModal:(e,d)=>{s(e),c(d)},closeModal:e=>{console.log(e),s(e),l(!1)},data:n,isImageModelOpen:a}};export{u};
